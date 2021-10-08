fe1ffbbf288ea2ebd319f3609f9be29809d8ff9e
gongzq5 <765194873@qq.com>
2021-10-08 08:33:33 +0800

    example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/autobuilding
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var logo string = `    ___         __           ____        _ __    ___            
+   /   | __  __/ /_____     / __ )__  __(_) /___/ (_)___  ____ _
+  / /| |/ / / / __/ __ \   / __  / / / / / / __  / / __ \/ __ ·/
+ / ___ / /_/ / /_/ /_/ /  / /_/ / /_/ / / / /_/ / / / / / /_/ / 
+/_/  |_\__,_/\__/\____/  /_____/\__,_/_/_/\__,_/_/_/ /_/\__, /  
+                                                       /____/   `
+
+func main() {
+	fmt.Println(logo)
+	fmt.Println("Hello, this is auto building testing repo from GZQ.")
+	fmt.Println("It is be so happy you can see this line.")
+	fmt.Println("Because it shows that at least you can run the main.")
+}
